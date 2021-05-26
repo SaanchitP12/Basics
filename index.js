474819ac03d8483b9952186f73b4419dda205aeb
SaanchitP12 <saanchitpillai.12@gmail.com>
2021-05-26 17:25:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+console.log("Hello World")
+console.log(2+1)
+console.log("2"+1 ) 
+
+let Fruits= ["orange", "mango", "banana", "apple"];
+console.log(Fruits[2])
+
+let name = 'Saanchit';
+
+const Job = 'Trader';
+console.log('My Name is ' + name + ' And I am a ' + Job)
+
+name = 'Saanchit Pillai';
+console.log('My Name is ' + name)
